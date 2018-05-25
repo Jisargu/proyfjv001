d693730fe43b2674a4ee692616a47fcd47d95921
Jisargu <jonathnsaa@gmail.com>
2018-05-25 14:47:20 -0500

    box shadow y creacion de varios elementos

---
+++ src/app/misiones/misiones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-misiones',
+  templateUrl: './misiones.component.html',
+  styleUrls: ['./misiones.component.scss']
+})
+export class MisionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
